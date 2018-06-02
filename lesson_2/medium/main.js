8bbe736996c0d03c3ecf797c85a9eb3f88124649
RomanovDm <79024081313@yandex.ru>
2018-06-03 01:16:01 +0300

    day

--- lesson_2/medium/main.js
+++ lesson_2/medium/main.js
@@ -1,18 +1,39 @@
-let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+/*let week = ["Воскресенье","Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
 
-let a = "";
+let a = ""; //куда формируется строка для вывода
 
 for (var i = 0; i <= week.length - 1; i++) {
-	switch ( week[i]) {
-		case "Четверг":
+	switch (i) {
+		case ((new Date().getDay() && 0) || (new Date().getDay() && 6)):
+			a += "<i><b>" + week[i] + "</b></i><br>";
+			break;
+		case new Date().getDay():
 			a += "<i>" + week[i] + "</i><br>";
 			break;
-		case "Суббота":
+		case 6 || 0:
 			a += "<b>" + week[i] + "</b><br>";
+			break;	
+		default:
+			a += week[i] + "<br>";
 			break;
-		case "Воскресенье":
+	}
+}*/
+
+let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота","Воскресенье"];
+let dat = new Date().getDay();
+let a = ""; //куда формируется строка для вывода
+
+for (var i = 0; i <= week.length - 1; i++) {
+	switch (i+1) {
+		case ((dat == 0) && 7) || (dat && 6):
+			a += "<i><b>" + week[i] + "</b></i><br>";
+			break;
+		case dat:
+			a += "<i>" + week[i] + "</i><br>";
+			break;
+		case 6 || 7:
 			a += "<b>" + week[i] + "</b><br>";
-			break;		
+			break;	
 		default:
 			a += week[i] + "<br>";
 			break;
